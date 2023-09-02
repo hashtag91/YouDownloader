d5867ff51979d3861a11d4123d89de56dd19ff39
hashtag91 <camarayacouba91@gmail.com>
2023-09-02 22:05:37 +0000

    Ajout de l'option 'type de video' à l'interface et la creation de la classe 'Download' dans le document YouDownloader.py pour la récuperation des index itag

--- YouDownloader.py
+++ YouDownloader.py
@@ -1,1 +1,18 @@
 from pytube import YouTube
+
+class Download:
+    def __init__(self,url):
+        self.url = url
+        self.youtube_video = YouTube(self.url)
+        self.streams = self.youtube_video.streams
+        self.VideoTitle()
+    def VideoTitle(self):
+        self.titleText = self.youtube_video.title
+        return self.titleText
+    def itagList(self):
+        itags = self.streams.itag_index
+        for i in itags:
+            print(i.real)
+
+yt = Download("https://www.youtube.com/watch?v=RmyUEEUFU3o")
+yt.itagList()
